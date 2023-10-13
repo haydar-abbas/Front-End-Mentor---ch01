c9efae894d84d625753bfc7a824dbc73662c6c41
haydar-abbas <haydarabbas40@proton.me>
2023-10-13 19:01:48 +0300

    First Commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const emailInput = document.getElementById("in_email");
+const formBtn = document.querySelector("form button");
+const dicmissBtn = document.getElementById("dismiss-btn");
+const successMobile = document.querySelector(".success-mobile");
+const signUpForm = document.querySelector(".sign-up-form");
+const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+formBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (emailInput.value != "") {
+    if (!emailInput.value.match(re)) {
+      document.querySelector(".error").style = "display: block";
+      emailInput.classList.add("error-style");
+    } else {
+      successMobile.classList.remove("hidden");
+      signUpForm.classList.add("hidden");
+    }
+  } else {
+    document.querySelector(".error").style = "display: block";
+    emailInput.classList.add("error-style");
+  }
+});
+
+dicmissBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  successMobile.classList.add("hidden");
+  signUpForm.classList.remove("hidden");
+});
